f1c7c7ed1c4de2ba51994fe9504bd3fa76511234
leviathanaxeislit <40699556+leviathanaxeislit@users.noreply.github.com>
2018-09-10 12:41:29 +0530

    For Darkseid

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,6 +19,39 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 from tg_bot.modules.translations.strings import tld, tld_help
 
+PM_START_TEXT = """
+Hello {}, my name is {}!
+
+I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
+python-telegram-bot library, and am fully opensource - you can find what makes me tick \
+[here](https://github.com/leviathanaxeislit/Darkseid-Bot)!
+
+You can find the list of available commands with /help.
+
+[Click here to add me in your groups](https://telegram.me/darkseid_bot?startgroup=add)
+
+If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+"""
+
+HELP_STRINGS = """
+Hey there! My name is *{}*.
+I'm a modular group management bot with a few fun extra commands ! Have a look at the following for an idea of some of \
+the things I can help you with.
+
+*Main* commands available:
+ - /start: start the bot
+ - /help: PM's you this message.
+ - /help <module name>: PM's you info about that module.
+ - /donate: information about how to donate!
+ - /settings:
+   - in PM: will send you your settings for all supported modules.
+   - in a group: will redirect you to pm, with all that chat's settings.
+
+{}
+And the following:
+""".format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+
+
 DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \. 
